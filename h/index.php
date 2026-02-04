4cba3e2ae5ca634ba899653f90ce8f22c31d40a0
ananya848 <imdevilslucifer666@gmail.com>
2026-02-04 15:03:08 +0700

    1

--- h/index.php
+++ h/index.php
@@ -15,7 +15,17 @@
 <form method="post" action="">
 Username <input type="text" name="auser" autofocus required> <br>
 Password <input type="password" name="apwd" required> <br>
-<button type="submit" name="Submit">LOGIN</button>
+<button type="submit" name="Submit">LOGIN</button></form>
+<?php
+if(isset($_POST['Submit'])){
+    include_once("connectdb.php");
+    $sql = "SELECT * FROM admin WHERE a_username='{$_POST['auser']}'AND a_password='{$_POST['apwd']}'LIMIT 1";
+    $rs = mysqli.query($conn,$sql);
+    $num = mysqli_num_rows($rs);
+
+    echo $num;
+}
+?>
 </body>
 </html>
 </title>
