25e4d99fabe5f8676484865e2ca69f6199cbac43
ananya848 <imdevilslucifer666@gmail.com>
2026-02-04 14:40:16 +0700

    1

---
+++ h/index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document<<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>อนัญญา ผลจันทร์ (ตาล)</title>
+</head>
+
+<body>
+<h1>หน้าเข้าสู่ระบบ - อนัญญา</h1>
+
+<form method="post" action="">
+Username <input type="text" name="auser" autofocus required> <br>
+Password <input type="password" name="apwd" required> <br>
+<button type="submit" name="Submit">LOGIN</button>
+</body>
+</html>
+/title>
+</head>
+
+<body>
+</body>
+</html>
