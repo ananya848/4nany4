44dcd14d52cdce9ecb1d7be09aaa8e9e65e3e457
4nany4 <imdevilslucifer666@gmail.com>
2026-02-17 20:50:34 +0700

    1

--- i/connectdb.php
+++ i/connectdb.php
@@ -1,7 +1,7 @@
 <?php
 		$host = "localhost";
 		$user = "root";
-		$pwd = "666";
+		$pwd = "";
 		$db = "4023db";
 		$conn = mysqli_connect($host, $user, $pwd, $db) or die ("เชื่อมต่อฐานข้อมูลไม่ได้");
 		mysqli_query($conn, "SET NAMES utf8");
